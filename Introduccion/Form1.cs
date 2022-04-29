80375b37212c4900d6143b5a09e58312647a7267
Ricardo Parga <20201020@uaz.edu.mx>
2022-04-29 00:03:15 -0500

    Finalizacion

--- Introduccion/Form1.cs
+++ Introduccion/Form1.cs
@@ -4,12 +4,12 @@ namespace Introduccion;
 public partial class Form1 : Form
 {
     //Primero agregamos las etiquetas con sus respectivos campos de texto de cada moneda a convertir
-    Label? lblMXM;
+    Label? lblMXN;
     Label? lblUSD;
     Label? lblCAD;
     Label? lblEUR;
     Label? lblJPY;
-    TextBox? txtMXM;
+    TextBox? txtMXN;
     TextBox? txtUSD;
     TextBox? txtCAD;
     TextBox? txtEUR;
@@ -30,6 +30,246 @@ public partial class Form1 : Form
     }
 
     public void InicializarComponentes(){
-        
+        this.Size = new Size(300, 350);
+        //Moneda MXN
+        lblMXN = new Label();
+        lblMXN.Text = "Peso mexicano";
+        lblMXN.AutoSize = true;
+        lblMXN.Location = new Point(10, 100);
+        lblMXN.Visible = false;
+        txtMXN = new TextBox();
+        txtMXN.Size = new Size(100, 20);
+        txtMXN.Location = new Point(150, 120);
+        txtMXN.Visible = false;
+        //Dolar USD
+        lblUSD = new Label();
+        lblUSD.Text = "Dolar estadounidense";
+        lblUSD.AutoSize = true;
+        lblUSD.Location = new Point(10, 120);
+        lblUSD.Visible = false;
+        txtUSD = new TextBox();
+        txtUSD.Size = new Size(100, 20);
+        txtUSD.Location = new Point(150, 135);
+        txtUSD.Visible = false;
+        //Dolar CAD
+        lblCAD = new Label();
+        lblCAD.Text = "Dolar canadiense";
+        lblCAD.AutoSize = true;
+        lblCAD.Location = new Point(10, 140);
+        lblCAD.Visible = false;
+        txtCAD = new TextBox();
+        txtCAD.Size = new Size(100, 20);
+        txtCAD.Location = new Point(150, 150);
+        txtCAD.Visible = false;
+        //Euro EUR
+        lblEUR = new Label();
+        lblEUR.Text = "Euro";
+        lblEUR.AutoSize = true;
+        lblEUR.Location = new Point(10, 160);
+        lblEUR.Visible = false;
+        txtEUR = new TextBox();
+        txtEUR.Size = new Size(100, 20);
+        txtEUR.Location = new Point(150, 175);
+        txtEUR.Visible = false;
+        //Yen JPY
+        lblJPY = new Label();
+        lblJPY.Text = "Yen japones";
+        lblJPY.AutoSize = true;
+        lblJPY.Location = new Point(10, 180);
+        lblJPY.Visible = false;
+        txtJPY = new TextBox();
+        txtJPY.Size = new Size(100, 20);
+        txtJPY.Location = new Point(150, 200);
+        txtJPY.Visible = false;
+        //Etiqueta Moneda
+        lblMoneda = new Label();
+        lblMoneda.Text = "Moneda";
+        lblMoneda.AutoSize = true;
+        lblMoneda.Location = new Point(10, 10);
+        //Combobox donde agregamos los tipos de monedas que estan disponibles
+        cmbMonedas = new ComboBox();
+        cmbMonedas.Size = new Size(160, 20);
+        cmbMonedas.Items.Add("Peso mexicano");
+        cmbMonedas.Items.Add("Dolar estadounidense");
+        cmbMonedas.Items.Add("Dolar canadiense");
+        cmbMonedas.Items.Add("Euro");
+        cmbMonedas.Items.Add("Yen japones");
+        cmbMonedas.SelectedValueChanged += new EventHandler(cmbMonedas_ValueChanged);
+        cmbMonedas.Location = new Point(10, 40);
+        //Etiqueta Monto
+        lblMonto = new Label();
+        lblMonto.Text = "Monto";
+        lblMonto.AutoSize = true;
+        lblMonto.Location = new Point(175, 10);
+        //TextBox Monto
+        txtMonto = new TextBox();
+        txtMonto.Text = "";
+        txtMonto.Size = new Size(100, 20);
+        txtMonto.Location = new Point(175, 30);
+        //Boton Calcular al que le pasamos el evento para accionar
+        btnCalcular = new Button();
+        btnCalcular.Text = "Calcular";
+        btnCalcular.Size = new Size(100, 25);
+        btnCalcular.Location = new Point(175, 77);
+        btnCalcular.Click += new EventHandler(btnVentana_Click);
+        //Etiqueta Conversion
+        lblConversiones = new Label();
+        lblConversiones.Text = "Conversion";
+        lblConversiones.AutoSize = true;
+        lblConversiones.Location = new Point(10, 110);
+
+        this.Controls.Add(lblMXN);
+        this.Controls.Add(lblUSD);
+        this.Controls.Add(lblCAD);
+        this.Controls.Add(lblEUR);
+        this.Controls.Add(lblJPY);
+        this.Controls.Add(txtMXN);
+        this.Controls.Add(txtUSD);
+        this.Controls.Add(txtCAD);
+        this.Controls.Add(txtEUR);
+        this.Controls.Add(txtJPY);
+        this.Controls.Add(lblMoneda);
+        this.Controls.Add(cmbMoneda);
+        this.Controls.Add(lblMonto);
+        this.Controls.Add(txtMonto);
+        this.Controls.Add(btnCalcular);
+        this.Controls.Add(lblConversion);
+    }
+
+    private void btnVentana_Click(Object sender, EventArgs e) {
+        Form2 formVentana = new Form2(monedaEscogida);
+
+        if (formVentana.ShowDialog() == DialogResult.OK) {
+            txtMXN.Visible = false;
+            lblMXN.Visible = false;
+            txtUSD.Visible = false;
+            lblUSD.Visible = false;
+            txtCAD.Visible = false;
+            lblCAD.Visible = false;
+            txtEUR.Visible = false;
+            lblEUR.Visible = false;
+            txtJPY.Visible = false;
+            lblJPY.Visible = false;
+
+            Label lblAgregado = new Label();
+            lblAgregado.Text = formVentana.lblMensaje.Text;
+            lblAgregado.AutoSize = true;
+            lblAgregado.Location = new Point(10, 20);
+            lblAgregado.Visible = false;
+
+            if (txtMonto.Text != null && double.Parse(txtMonto.Text) > 0) {
+                if (formVentana.MXN.Checked == true) {
+                    txtMXN.Visible = true;
+                    lblMXN.Visible = true;
+                    if (monedaEscogida == "USD") {
+                        txtMXN.Text = "$" + (double.Parse(txtMonto.Text) * 21.23).ToString();
+                    }
+                    if (monedaEscogida == "CAD") {
+                        txtMXN.Text = "$" + (double.Parse(txtMonto.Text) * 16.55).ToString();
+                    }
+                    if (monedaEscogida == "EUR") {
+                        txtMXN.Text = "$" + (double.Parse(txtMonto.Text) * 23.96).ToString();
+                    }
+                    if (monedaEscogida == "JPY") {
+                        txtMXN.Text = "$" + (double.Parse(txtMonto.Text) * 0.1878).ToString();
+                    }
+                }
+
+                if (formVentana.USD.Checked == true) {
+                    txtUSD.Visible = true;
+                    lblUSD.Visible = true;
+                    if (monedaEscogida == "MXN") {
+                        txtUSD.Text = "$" + (double.Parse(txtMonto.Text) * 0.05).ToString();
+                    }
+                    if (monedaEscogida == "CAD") {
+                        txtUSD.Text = "$" + (double.Parse(txtMonto.Text) * 0.78).ToString();
+                    }
+                    if (monedaEscogida == "EUR") {
+                        txtUSD.Text = "$" + (double.Parse(txtMonto.Text) * 1.13).ToString();
+                    }
+                    if (monedaEscogida == "JPY") {
+                        txtUSD.Text = "$" + (double.Parse(txtMonto.Text) * 0.0088).ToString();
+                    }
+                }
+
+                if (formVentana.CAD.Checked == true) {
+                    txtCAD.Visible = true;
+                    lblCAD.Visible = true;
+                    if (monedaEscogida == "USD") {
+                        txtCAD.Text = "$" + (double.Parse(txtMonto.Text) * 1.28).ToString();
+                    }
+                    if (monedaEscogida == "MXN") {
+                        txtCAD.Text = "$" + (double.Parse(txtMonto.Text) * 0.06).ToString();
+                    }
+                    if (monedaEscogida == "EUR") {
+                        txtCAD.Text = "$" + (double.Parse(txtMonto.Text) * 1.45).ToString();
+                    }
+                    if (monedaEscogida == "JPY") {
+                        txtCAD.Text = "$" + (double.Parse(txtMonto.Text) * 0.0113).ToString();
+                    }
+                }
+
+                if (formVentana.EUR.Checked == true) {
+                    txtEUR.Visible = true;
+                    lblEUR.Visible = true;
+                    if (monedaEscogida == "USD") {
+                        txtEUR.Text = "€" + (double.Parse(txtMonto.Text) * 0.89).ToString();
+                    }
+                    if (monedaEscogida == "CAD") {
+                        txtEUR.Text = "€" + (double.Parse(txtMonto.Text) * 0.69).ToString();
+                    }
+                    if (monedaEscogida == "MXN") {
+                        txtEUR.Text = "€" + (double.Parse(txtMonto.Text) * 0.04).ToString();
+                    }
+                    if (monedaEscogida == "JPY") {
+                        txtEUR.Text = "€" + (double.Parse(txtMonto.Text) * 0.0078).ToString();
+                    }
+                }
+
+                if (formVentana.JPY.Checked == true) {
+                    txtJPY.Visible = true;
+                    lblJPY.Visible = true;
+                    if (monedaEscogida == "USD") {
+                        txtJPY.Text = "¥" + (double.Parse(txtMonto.Text) * 113.05).ToString();
+                    }
+                    if (monedaEscogida == "CAD") {
+                        txtJPY.Text = "¥" + (double.Parse(txtMonto.Text) * 88.12).ToString();
+                    }
+                    if (monedaEscogida == "EUR") {
+                        txtJPY.Text = "¥" + (double.Parse(txtMonto.Text) * 127.56).ToString();
+                    }
+                    if (monedaEscogida == "MXN") {
+                        txtJPY.Text = "¥" + (double.Parse(txtMonto.Text) * 5.32).ToString();
+                    }
+                }
+            }
+            this.Controls.Add(lblAgregado);
+        }
+    }
+
+    private void cmbMonedas_ValueChanged(object sender, EventArgs e) {
+        if (cmbMonedas.SelectedItem != null && txtMonto != null) {
+            
+            if (cmbMonedas.SelectedItem.ToString() == "Peso mexicano") {
+                btnCalcular.Visible = true;
+                monedaEscogida = "MXN";
+            }
+            else if (cmbMonedas.SelectedItem.ToString() == "Dolar estadounidense") {
+                btnCalcular.Visible = true;
+                monedaEscogida = "USD";
+            }
+            else if (cmbMonedas.SelectedItem.ToString() == "Dolar canadiense") {
+                btnCalcular.Visible = true;
+                monedaEscogida = "CAD";
+            }
+            else if (cmbMonedas.SelectedItem.ToString() == "Euro") {
+                btnCalcular.Visible = true;
+                monedaEscogida = "EUR";
+            }
+            else if (cmbMonedas.SelectedItem.ToString() == "Yen japones") {
+                btnCalcular.Visible = true;
+                monedaEscogida = "JPY";
+            }
+        }
     }
 }
--- Introduccion/Form2.cs
+++ Introduccion/Form2.cs
@@ -1,9 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace Introduccion;
 
-public partial class Form2 : Form
-{
-    public Form2()
-    {
+public partial class Form2 : Form {
+    public Label lblMensaje;
+    public CheckBox MXN;
+    public CheckBox USD;
+    public CheckBox CAD;
+    public CheckBox EUR;
+    public CheckBox JPY;
+    public Boolean MXNestatus = false;
+    public Boolean USDestatus = false;
+    public Boolean CADestatus = false;
+    public Boolean EURestatus = false;
+    public Boolean JPYestatus = false;
+    public string _monedaEscogida;
+    private Button btnCerrar;
+
+    public Form2(string monedaEscogida) {
+        this._monedaEscogida = monedaEscogida;
+        lblMensaje = new Label();
+        btnCerrar = new Button();
         InitializeComponent();
+        inicializarComponentes();  
+    }
+
+    private void inicializarComponentes() {
+        this.Size = new Size(300, 230);
+        lblMensaje.Text = "";
+        lblMensaje.AutoSize = true;
+        lblMensaje.Location = new Point(80, 80);
+
+        //Boton Cerrar
+        btnCerrar.Text = "Cerrar";
+        btnCerrar.AutoSize = true;
+        btnCerrar.Location = new Point(100, 160);
+        btnCerrar.Click += new EventHandler(btnCerrarVentana_Click);
+
+        //Peso MXN
+        MXN = new CheckBox();
+        MXN.AutoSize = true;
+        MXN.Location = new Point(10, 10);
+        MXN.Text = ("Peso mexicano");
+        if (_monedaEscogida != "MXN") {
+            this.Controls.Add(MXN);
+        }
+
+        //Dolar USD
+        USD = new CheckBox();
+        USD.AutoSize = true;
+        USD.Location = new Point(10, 40);
+        USD.Text = ("Dolar estadounidense");
+        if (_monedaEscogida != "USD") {
+            this.Controls.Add(USD);
+        }
+
+        //Dolar CAD
+        CAD = new CheckBox();
+        CAD.AutoSize = true;
+        CAD.Location = new Point(10, 70);
+        CAD.Text = ("Dolar canadiense");
+        if (_monedaEscogida != "CAD") {
+            this.Controls.Add(CAD);
+        }
+
+        //Euro EUR
+        EUR = new CheckBox();
+        EUR.AutoSize = true;
+        EUR.Location = new Point(10, 100);
+        EUR.Text = ("Euro");
+        if (_monedaEscogida != "EUR") {
+            this.Controls.Add(EUR);
+        }
+
+        //Yen JPY
+        JPY = new CheckBox();
+        JPY.AutoSize = true;
+        JPY.Location = new Point(10, 130);
+        JPY.Text = ("Yen japones");
+        if (_monedaEscogida != "JPY") {
+            this.Controls.Add(JPY);
+        }
+        this.Controls.Add(lblMensaje);
+        this.Controls.Add(btnCerrar);
+    }
+
+    private void btnCerrarVentana_Click(Object sender, EventArgs e) {
+        this.DialogResult = DialogResult.OK;
+        this.Close();
     }
 }
